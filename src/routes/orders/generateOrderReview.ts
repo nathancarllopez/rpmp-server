fe259843978dd8046584bc3962142769d7574a0f
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-22 11:40:49 -0500

    Generates a pdf for the user to review on the client side to make sure their order was uploaded and parsed correctly

---
+++ src/routes/orders/generateOrderReview.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import PDFDocumentWithTables from "pdfkit-table";
+import { Meal, Order, OrderError, OrderStatistics } from "../../types/rpmp-types";
+import { addErrors, addMeals, addOrders, addSummary } from "./addPageHelpers";
+
+export default async function generateOrderReview(
+  req: Request,
+  res: Response
+): Promise<void> {
+  const {
+    meals,
+    orders,
+    orderErrors,
+    stats,
+  }: {
+    meals: Meal[];
+    orders: Order[];
+    orderErrors: OrderError[];
+    stats: OrderStatistics;
+  } = req.body;
+
+  try {
+    const report = new PDFDocumentWithTables({ layout: "landscape" });
+
+    if (orderErrors.length > 0) {
+      addErrors(report, orderErrors);
+      report.addPage();
+    }
+
+    addSummary(report, stats);
+    report.addPage();
+    addOrders(report, orders);
+    report.addPage();
+    addMeals(report, meals);
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "attachment");
+
+    report.pipe(res);
+    report.end();
+  } catch (error) {
+    console.log(error);
+  }
+}
