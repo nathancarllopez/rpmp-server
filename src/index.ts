b75eb0273ad1390756ad280d9df9669ec5d19c3a
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-22 11:37:28 -0500

    Added auth, orders, and timecards route imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-// import authRoutes from "./routes/auth/route";
-// import ordersRoutes from "./routes/orders/route";
-// import timecardsRoutes from "./routes/timecards/route"
+import authRoutes from "./routes/auth/route";
+import ordersRoutes from "./routes/orders/route";
+import timecardsRoutes from "./routes/timecards/route"
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,14 +11,14 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-// app.use("/auth", authRoutes);
-// app.use("/orders", ordersRoutes);
-// app.use("/timecards", timecardsRoutes);
+app.use("/auth", authRoutes);
+app.use("/orders", ordersRoutes);
+app.use("/timecards", timecardsRoutes);
 
 app.get("/health", (_, res) => {
   res.send("Hello from the backend!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
